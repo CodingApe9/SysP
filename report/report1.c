5f722ab5326ff55c30bcdc2371bdda9f42ba4fa0
asusNoteBook <pos06098@gmail.com>
2023-04-05 16:30:03 +0900

    ch report1.c

--- report/report1.c
+++ report/report1.c
@@ -3,26 +3,30 @@
 
 int main(void){
 		
-		int x;
-			
-			printf("16진수 숫자 입력: ");
-				scanf("%x", &x); 
-					
-					for (int i = 15; i > 11; i--) {
-						        int result = x >> i & 1;
-							        printf("%d", result);
-								    }
-					    printf(" : ");
-					        for (int i = 11; i > 8; i--) {
-							        int result = x >> i & 1;
-								        printf("%d", result);
-									    }
-						    printf(" : ");
-						        for (int i = 8; i >= 0; i--) {
-								        int result = x >> i & 1;
-									        printf("%d", result);
-										    }
-							    printf("\n");
-							    	
-							    	return 0;
+	int x;
+	printf("16진수 숫자 입력: ");
+	scanf("%x", &x); 
+	
+	for (int i = 15; i > 11; i--) {
+		int result = x >> i & 1;
+		printf("%d", result);
+	}
+	printf(" : ");
+	
+	for (int i = 11; i > 8; i--) {
+
+		int result = x >> i & 1;
+
+		printf("%d", result);
+
+	}
+	printf(" : ");
+	
+	for (int i = 8; i >= 0; i--) {
+		int result = x >> i & 1;
+		printf("%d", result);
+	}
+	printf("\n");
+	
+	return 0;
 }
