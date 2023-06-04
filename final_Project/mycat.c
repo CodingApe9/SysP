7c5b996713ed17e98b04d707888e9d74fbec2d2d
CodingApe9 <tlqkf6974@>
2023-06-04 20:52:53 +0900

    new cat with option "n, s, e, t, T"

--- final_Project/mycat.c
+++ final_Project/mycat.c
@@ -1,91 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <getopt.h>
+#include <fcntl.h>
 
-void cat_file(FILE* file, int show_line_numbers, int show_nonblank_numbers, int squeeze_blank_lines, int show_ends, int show_tabs) {
+#define BUFFER_SIZE 4096
+
+void display_file(const char* filename, int number_lines, int show_ends, int squeeze_blank, int show_tabs) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("fopen");
+        return;
+    }
+
+    char buffer[BUFFER_SIZE];
     int line_number = 1;
-    int prev_blank_line = 0;
-    char buffer[1024];
+    int blank_line_count = 0;
+    int prev_blank = 0;
+    int prev_newline = 0;
 
-    while (fgets(buffer, sizeof(buffer), file)) {
-        if (squeeze_blank_lines && prev_blank_line && buffer[0] == '\n') {
-            continue;
-        }
-
-        if (show_line_numbers) {
-            printf("%6d  ", line_number++);
-        } else if (show_nonblank_numbers && buffer[0] != '\n') {
-            printf("%6d  ", line_number++);
-            prev_blank_line = 0;
+    while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
+        if (squeeze_blank && buffer[0] == '\n') {
+            if (prev_blank) {
+                continue;
+            } else {
+                prev_blank = 1;
+            }
         } else {
-            prev_blank_line = (buffer[0] == '\n');
+            prev_blank = 0;
         }
 
-        printf("%s", buffer);
+        if (number_lines) {
+            printf("%6d  ", line_number);
+        }
+
+        for (int i = 0; buffer[i] != '\0'; i++) {
+            if (show_tabs && buffer[i] == '\t') {
+                printf("^I");
+            } else {
+                putchar(buffer[i]);
+            }
+        }
 
         if (show_ends && buffer[strlen(buffer) - 1] != '\n') {
             printf("$");
         }
 
-        if (show_tabs) {
-            for (int i = 0; buffer[i] != '\0'; i++) {
-                if (buffer[i] == '\t') {
-                    printf("^I");
-                } else {
-                    putchar(buffer[i]);
-                }
-            }
-        }
+        line_number++;
     }
+
+    fclose(file);
 }
 
 int main(int argc, char* argv[]) {
-    int show_line_numbers = 0;
-    int show_nonblank_numbers = 0;
-    int squeeze_blank_lines = 0;
+    int number_lines = 0;
     int show_ends = 0;
+    int squeeze_blank = 0;
     int show_tabs = 0;
 
     int opt;
-    while ((opt = getopt(argc, argv, "nbsET")) != -1) {
+    while ((opt = getopt(argc, argv, "nsetT")) != -1) {
         switch (opt) {
             case 'n':
-                show_line_numbers = 1;
-                break;
-            case 'b':
-                show_nonblank_numbers = 1;
+                number_lines = 1;
                 break;
             case 's':
-                squeeze_blank_lines = 1;
+                squeeze_blank = 1;
                 break;
-            case 'E':
+            case 'e':
                 show_ends = 1;
                 break;
-            case 'T':
+            case 't':
                 show_tabs = 1;
                 break;
+            case 'T':
+                show_tabs = 0;
+                break;
             default:
-                fprintf(stderr, "Usage: cat [-n] [-b] [-s] [-E] [-T] [file ...]\n");
+                fprintf(stderr, "Usage: cat [-n] [-s] [-e] [-t|-T] [file...]\n");
                 return 1;
         }
     }
 
-    if (optind >= argc) {
-        // 파일이 지정되지 않은 경우 표준 입력을 읽어옴
-        cat_file(stdin, show_line_numbers, show_nonblank_numbers, squeeze_blank_lines, show_ends, show_tabs);
+    if (optind == argc) {
+        // No files specified, read from stdin
+        char buffer[BUFFER_SIZE];
+        while (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
+            printf("%s", buffer);
+        }
     } else {
-        // 지정된 파일들을 차례대로 읽어옴
+        // Read files
         for (int i = optind; i < argc; i++) {
-            FILE* file = fopen(argv[i], "r");
-            if (file == NULL) {
-                perror("fopen");
-                continue;
-            }
-
-            cat_file(file, show_line_numbers, show_nonblank_numbers, squeeze_blank_lines, show_ends, show_tabs);
-
-            fclose(file);
+            display_file(argv[i], number_lines, show_ends, squeeze_blank, show_tabs);
         }
     }
 
