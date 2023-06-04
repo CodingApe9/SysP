6f15b0ad6f39087c139b457e8cc1c1e89a295b0b
CodingApe9 <tlqkf6974@>
2023-06-04 19:14:40 +0900

    refact

--- final_Project/myls.c
+++ final_Project/myls.c
@@ -7,6 +7,91 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <grp.h>
+#include <string.h>
+
+void print_file_info(struct dirent *de, struct stat *file_stat, int show_details, int show_inode, int omit_group, int show_disk_usage, int show_file_type) {
+    struct passwd *pwd;
+    struct group *grp;
+    struct tm *time_info;
+    char timestamp[20];
+
+    if (show_details) {
+        pwd = getpwuid(file_stat->st_uid);
+        grp = getgrgid(file_stat->st_gid);
+        time_info = localtime(&(file_stat->st_mtime));
+        strftime(timestamp, sizeof(timestamp), "%b %e %H:%M", time_info);
+        printf("%s ", (S_ISDIR(file_stat->st_mode) ? "d" : "-"));
+
+        if (show_inode) {
+            printf("%lu ", file_stat->st_ino);
+        }
+
+        printf("%2lu ", file_stat->st_nlink);
+
+        if (!omit_group) {
+            printf("%s ", pwd->pw_name);
+        }
+
+        if (!omit_group) {
+            printf("%s ", grp->gr_name);
+        }
+
+        if (show_disk_usage) {
+            printf("%8lld ", (long long)file_stat->st_blocks * 512 / 1024);
+        } else {
+            printf("%8lld ", (long long)file_stat->st_size / 1024);
+        }
+
+        printf("%s ", timestamp);
+
+        if (show_file_type) {
+            if (S_ISDIR(file_stat->st_mode)) {
+                printf("/");
+            } else if (file_stat->st_mode & S_IXUSR || file_stat->st_mode & S_IXGRP || file_stat->st_mode & S_IXOTH) {
+                printf("*");
+            }
+        }
+
+        printf("%s\n", de->d_name);
+    } else {
+        printf("%s\n", de->d_name);
+    }
+}
+
+void ls_recursive(const char *directory, int show_hidden, int show_details, int show_inode, int omit_group, int show_disk_usage, int show_file_type) {
+    struct dirent *de;
+    DIR *dir = opendir(directory);
+
+    if (dir == NULL) {
+        printf("디렉토리를 열 수 없습니다.\n");
+        return;
+    }
+
+    struct stat file_stat;
+    char subdirectory[256];
+
+    while ((de = readdir(dir)) != NULL) {
+        if (!show_hidden && de->d_name[0] == '.') {
+            continue;
+        }
+
+        sprintf(subdirectory, "%s/%s", directory, de->d_name);
+
+        if (stat(subdirectory, &file_stat) == -1) {
+            printf("파일 정보를 가져올 수 없습니다.\n");
+            return;
+        }
+
+        print_file_info(de, &file_stat, show_details, show_inode, omit_group, show_disk_usage, show_file_type);
+
+        if (S_ISDIR(file_stat.st_mode) && strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0) {
+            printf("\n%s:\n", subdirectory);
+            ls_recursive(subdirectory, show_hidden, show_details, show_inode, omit_group, show_disk_usage, show_file_type);
+        }
+    }
+
+    closedir(dir);
+}
 
 int main(int argc, char* argv[]) {
     int show_hidden = 0;
@@ -67,10 +152,6 @@ int main(int argc, char* argv[]) {
     }
 
     struct stat file_stat;
-    struct passwd *pwd;
-    struct group *grp;
-    struct tm *time_info;
-    char timestamp[20];
 
     while ((de = readdir(dir)) != NULL) {
         if (!show_hidden && de->d_name[0] == '.') {
@@ -82,60 +163,11 @@ int main(int argc, char* argv[]) {
             return 1;
         }
 
-        if (show_details) {
-            pwd = getpwuid(file_stat.st_uid);
-            grp = getgrgid(file_stat.st_gid);
-            time_info = localtime(&(file_stat.st_mtime));
-            strftime(timestamp, sizeof(timestamp), "%b %e %H:%M", time_info);
-            printf("%s ", (S_ISDIR(file_stat.st_mode) ? "d" : "-"));
-
-            if (show_inode) {
-                printf("%lu ", file_stat.st_ino);
-            }
-
-            printf("%2lu ", file_stat.st_nlink);
-
-            if (!omit_group) {
-                printf("%s ", pwd->pw_name);
-            }
-
-            if (!omit_group) {
-                printf("%s ", grp->gr_name);
-            }
-
-            if (show_disk_usage) {
-                printf("%8lld ", (long long)file_stat.st_blocks * 512);
-            } else {
-                printf("%8lld ", (long long)file_stat.st_size);
-            }
-
-            printf("%s ", timestamp);
-
-            if (show_file_type) {
-                if (S_ISDIR(file_stat.st_mode)) {
-                    printf("/");
-                } else if (file_stat.st_mode & S_IXUSR || file_stat.st_mode & S_IXGRP || file_stat.st_mode & S_IXOTH) {
-                    printf("*");
-                }
-            }
-
-            printf("%s\n", de->d_name);
-        } else {
-            printf("%s\n", de->d_name);
-        }
+        print_file_info(de, &file_stat, show_details, show_inode, omit_group, show_disk_usage, show_file_type);
 
         if (recursive && S_ISDIR(file_stat.st_mode) && strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0) {
             printf("\n%s:\n", de->d_name);
-            char subdirectory[256];
-            sprintf(subdirectory, "./%s", de->d_name);
-            DIR *subdir = opendir(subdirectory);
-            if (subdir != NULL) {
-                closedir(dir);
-                dir = subdir;
-            } else {
-                printf("하위 디렉토리를 열 수 없습니다.\n");
-                return 1;
-            }
+            ls_recursive(de->d_name, show_hidden, show_details, show_inode, omit_group, show_disk_usage, show_file_type);
         }
     }
 
