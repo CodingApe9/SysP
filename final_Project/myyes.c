124dcea3ef13f185b9d6ac569c99fb095502f737
CodingApe9 <tlqkf6974@>
2023-06-07 19:53:57 +0900

    fix myyes

--- final_Project/myyes.c
+++ final_Project/myyes.c
@@ -6,8 +6,9 @@ int main(int argc, char *argv[]) {
     if (argc > 1){;
         while (1){
             for (int i=1; i<argc; i++){
-                printf("%s\n", argv[i]);
+                printf("%s", argv[i]);
             }
+            printf("\n");
         }
     }
     else {
