8f35123cf2f74663537c070c27198cc3d8299bcd
CodingApe9 <tlqkf6974@>
2023-06-07 20:54:02 +0900

    fetch rev

--- final_Project/myrev.c
+++ final_Project/myrev.c
@@ -4,7 +4,7 @@
 
 // 문자열을 뒤집는 함수
 void reverse(char *str) {
-    unsigned int length = strlen(str);
+    unsigned int length = strlen(str-1);
     unsigned int i, j;
     for (i = 0, j = length - 1; i < j; i++, j--) {
         char temp = str[i];
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
 
         while ((read = getline(&line, &line_length, file)) != -1) {
         reverse(line);
-        printf("%s", line);
+        printf("%s\n", line);
     }
     }
     // 표준 입력에서 각 줄을 읽어서 뒤집어 출력
